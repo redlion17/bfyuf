fe0eebe79dc7bdae6b24a0cb730fea1983e87216
Yash_official <116334589+redlion17@users.noreply.github.com>
2024-08-17 14:27:02 +0530

    Add files via upload

---
+++ output.c
@@ -0,0 +1,24 @@
+/*
+Student_Number :PE/2022/034
+Student_Name   :WKY DILSHAN
+Created_Date   :07/08/2024
+Update_Date    :07/08/2024
+
+*/
+
+//Question No :04
+
+#include <stdio.h>
+int main() 
+{
+   int a = 25;
+   float b =45.0;
+   char c = 'A';
+   float sum;
+   sum = a+b+c;
+   printf("Result = %f\n",sum);
+   return 0;
+
+
+}
+
